16502ed2c604a46360d648fe2cd02799031fccff
Rémy Perona <remperona@gmail.com>
2025-08-13 14:51:08 -0400

    add setup for integration tests

---
+++ tests/Integration/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace WPMedia\Beta\Tests\Integration;
+
+define( 'WPM_BETA_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPM_BETA_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPM_BETA_TESTS_DIR', __DIR__ );
+define( 'WPM_BETA_IS_TESTING', true );
